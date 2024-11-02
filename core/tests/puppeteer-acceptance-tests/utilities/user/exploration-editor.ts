d497425561802ecb9e36b7a43f403839b4b08fef
Mohit Ruwatia <mohitruwatia2312@gmail.com>
2024-11-02 11:37:10 +0530

    Fixed a failing test

--- core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/exploration-editor.ts
@@ -1062,6 +1062,7 @@ export class ExplorationEditor extends BaseUser {
         await this.page.type(floatFormInput, answer);
         break;
       case 'Multiple Choice':
+        await this.page.waitForSelector(multipleChoiceResponseDropdown);
         await this.clickOn(multipleChoiceResponseDropdown);
         await this.page.waitForSelector(multipleChoiceResponseOption, {
           visible: true,
