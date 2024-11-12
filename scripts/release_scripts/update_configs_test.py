e910655fa846f54d3c879613d7d7019789283467
Oscar <oscar@cps1.tech>
2024-11-12 16:36:36 -0300

    Fix update_configs_test

--- scripts/release_scripts/update_configs_test.py
+++ scripts/release_scripts/update_configs_test.py
@@ -176,7 +176,8 @@ class UpdateConfigsTests(test_utils.GenericTestBase):
 
                 update_configs.update_app_yaml(
                     temp_app_yaml.name, temp_feconf_config.name)
-                
+
+                temp_app_yaml.seek(0)
                 app_yaml_text = temp_app_yaml.read()
                 self.assertIn(
                     'Access-Control-Allow-Origin: https://oppia.org',
