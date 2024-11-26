c8f8ac725ba952aab141e3f01b2846bb05f625be
medchouaib <mohamedchouaieb.abdelkefi@supcom.tn>
2024-11-26 17:17:20 +0100

    test_exploation_test3

--- core/domain/exp_services_test.py
+++ core/domain/exp_services_test.py
@@ -418,7 +418,9 @@ class ExplorationSummaryQueriesUnitTests(ExplorationServicesUnitTests):
         # Add a moderator user.
         self.signup(self.MODERATOR_EMAIL, self.MODERATOR_USERNAME)
         self.moderator_id = self.get_user_id_from_email(self.MODERATOR_EMAIL)
-        self.user_moderator = user_services.get_user_actions_info(self.moderator_id)
+        self.user_moderator = user_services.get_user_actions_info(
+            self.moderator_id
+        )
         # Setup the explorations to fit into 2 different categoriers and 2
         # different language groups. Also, ensure 2 of them have similar
         # titles.
@@ -665,7 +667,9 @@ class ExplorationSummaryQueriesUnitTests(ExplorationServicesUnitTests):
         )
         self.assertEqual(len(exp_ids), 4)
     def test_update_exploration_updates_summary(self) -> None:
-        """Test that updating the title or category of an exploration updates the ExpSummaryModel."""
+        """Test that updating the title or category of 
+        an exploration updates the ExpSummaryModel.
+        """
         self.save_new_default_exploration(
             self.EXP_0_ID,
             self.owner_id,
@@ -683,10 +687,14 @@ class ExplorationSummaryQueriesUnitTests(ExplorationServicesUnitTests):
             self.EXP_0_ID,
             change_list, 'Updated title.'
         )
-        updated_summary = exp_fetchers.get_exploration_summary_by_id(self.EXP_0_ID)
+        updated_summary = exp_fetchers.get_exploration_summary_by_id(
+            self.EXP_0_ID
+        )
         self.assertEqual(updated_summary.title, 'Updated Title')
     def test_revert_exploration_updates_summary(self) -> None:
-        """Test that reverting an exploration also restores the ExpSummaryModel."""
+        """Test that reverting an exploration
+          also restores the ExpSummaryModel.
+        """
         self.save_new_default_exploration(
             self.EXP_1_ID,
             self.owner_id,
@@ -703,7 +711,9 @@ class ExplorationSummaryQueriesUnitTests(ExplorationServicesUnitTests):
             change_list,
             'Second title update.'
         )
-        summary = exp_fetchers.get_exploration_summary_by_id(self.EXP_1_ID)
+        summary = exp_fetchers.get_exploration_summary_by_id(
+            self.EXP_1_ID
+        )
         self.assertEqual(summary.title, 'Second Title')
         exp = exp_fetchers.get_exploration_by_id(self.EXP_1_ID)
         exp_services.revert_exploration(
@@ -712,19 +722,23 @@ class ExplorationSummaryQueriesUnitTests(ExplorationServicesUnitTests):
             exp.version - 1
         )
 
-        reverted_summary = exp_fetchers.get_exploration_summary_by_id(self.EXP_1_ID)
+        reverted_summary = exp_fetchers.get_exploration_summary_by_id(
+            self.EXP_1_ID
+        )
         self.assertEqual(reverted_summary.title, 'First Title')
     def test_rights_change_updates_summary(self) -> None:
-        """Test that changing the access rights of an exploration updates its visibility in ExpSummaryModel."""
+        """Test that changing the access rights of an exploration
+          updates its visibility in ExpSummaryModel.
+        """
         self.save_new_default_exploration(self.EXP_2_ID, self.owner_id)
         rights_manager.publish_exploration(self.owner, self.EXP_2_ID)
         summary = exp_fetchers.get_exploration_summary_by_id(self.EXP_2_ID)
         self.assertTrue(summary.status == rights_domain.ACTIVITY_STATUS_PUBLIC)
         rights_manager.unpublish_exploration(self.user_moderator, self.EXP_2_ID)
         updated_summary = exp_fetchers.get_exploration_summary_by_id(self.EXP_2_ID)
-        self.assertTrue(updated_summary.status == rights_domain.ACTIVITY_STATUS_PRIVATE)
-
-
+        self.assertTrue(
+            updated_summary.status == rights_domain.ACTIVITY_STATUS_PRIVATE
+        )
 class ExplorationCreateAndDeleteUnitTests(ExplorationServicesUnitTests):
     """Test creation and deletion methods."""
 
