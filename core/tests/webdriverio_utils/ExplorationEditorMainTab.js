7ba1bc76c990032b3d55989b6a9da502c77a77e9
Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>
2024-11-30 17:31:32 +0530

    Trying to fix state name input e2e flake

--- core/tests/webdriverio_utils/ExplorationEditorMainTab.js
+++ core/tests/webdriverio_utils/ExplorationEditorMainTab.js
@@ -1051,11 +1051,8 @@ var ExplorationEditorMainTab = function () {
       stateNameContainer,
       'State name container is taking too long to become clickable.'
     );
-    // eslint-disable-next-line oppia/e2e-practices
-    await browser.pause(2000);
     await action.click('State Name Container', stateNameContainer);
-    // eslint-disable-next-line oppia/e2e-practices
-    await browser.pause(2000);
+    await waitFor.pageToFullyLoad();
     await waitFor.visibilityOf(
       stateNameInput,
       'State Name Input takes too long to appear'
