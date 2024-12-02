f3c08bed60656cfc0e67dd79dc5c51eb88b9a455
Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>
2024-12-02 11:50:51 +0530

    Updated variable name in insert script service spec file

--- core/templates/services/insert-script.service.spec.ts
+++ core/templates/services/insert-script.service.spec.ts
@@ -110,7 +110,7 @@ describe('InsertScriptService', () => {
     setTimeout(() => {
       expect(
         // eslint-disable-next-line dot-notation
-        insertScriptService['scriptsLoading'].has(KNOWN_SCRIPTS.DONORBOX)
+        insertScriptService['partiallyLoadedScripts'].has(KNOWN_SCRIPTS.DONORBOX)
       ).toBe(false);
       done();
     }, 20);
