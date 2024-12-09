80b44a639a793e5c58c8e8c3300a8d1c9741a5c7
Mohit <mohitruwatia2312@gmail.com>
2024-12-09 11:33:24 +0530

    Made changes to enable auto loading of MathJax

--- core/templates/directives/mathjax.directive.spec.ts
+++ core/templates/directives/mathjax.directive.spec.ts
@@ -15,6 +15,8 @@
 import {Component} from '@angular/core';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {MathJaxDirective} from './mathjax.directive';
+import {InsertScriptService} from 'services/insert-script.service';
+import {By} from '@angular/platform-browser';
 
 /**
  * @fileoverview Unit tests for mathjax directive
@@ -39,10 +41,17 @@ const mockMathJs = {
 describe('MathJax directive', () => {
   let component: MockCompA;
   let fixture: ComponentFixture<MockCompA>;
+  let mockInsertScriptService: jasmine.SpyObj<InsertScriptService>;
   const originalMathJax = window.MathJax;
   beforeEach(waitForAsync(() => {
+    mockInsertScriptService = jasmine.createSpyObj('InsertScriptService', [
+      'loadScript',
+    ]);
     TestBed.configureTestingModule({
       declarations: [MockCompA, MathJaxDirective],
+      providers: [
+        {provide: InsertScriptService, useValue: mockInsertScriptService},
+      ],
     }).compileComponents();
   }));
 
@@ -50,6 +59,13 @@ describe('MathJax directive', () => {
     fixture = TestBed.createComponent(MockCompA);
     component = fixture.componentInstance;
     window.MathJax = mockMathJs as unknown as typeof MathJax;
+    mockInsertScriptService.loadScript.and.callFake((script, callback) => {
+      // Simulate script loaded.
+      callback();
+    });
+
+    // Trigger Angular's change detection.
+    fixture.detectChanges();
   }));
 
   afterEach(() => {
@@ -60,6 +76,13 @@ describe('MathJax directive', () => {
     const spy = spyOn(mockMathJaxHub, 'Queue');
     component.expr = '/frac{z}{y}';
     fixture.detectChanges();
+
     expect(spy).toHaveBeenCalled();
+
+    const el = fixture.debugElement.query(By.directive(MathJaxDirective));
+
+    const scriptTag = el.nativeElement.querySelector('script[type="math/tex"]');
+    expect(scriptTag).not.toBeNull();
+    expect(scriptTag.textContent).toContain('/frac{z}{y}');
   }));
 });
