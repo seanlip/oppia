c5c89229b90dad388647bfc4d39d84185d8374b7
cs <cs.temporary@icloud.com>
2024-12-09 14:51:10 +0000

    Fix job imports test

--- scripts/linters/test_files/valid_job_imports.py
+++ scripts/linters/test_files/valid_job_imports.py
@@ -51,6 +51,8 @@ from core.jobs.batch_jobs import (                                   # pylint: d
 from core.jobs.batch_jobs import contributor_admin_stats_jobs        # pylint: disable=unused-import  # isort: skip
 from core.jobs.batch_jobs import (                                   # pylint: disable=unused-import  # isort: skip
     story_node_jobs)
+from core.jobs.batch_jobs import (                                   # pylint: disable=unused-import  # isort: skip
+    missing_translation_images_repair_jobs)
 
 class FakeClass:
     """This is a fake docstring for valid syntax purposes."""
