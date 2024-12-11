06549e59c035dd1d19d7e02fac858907277dae6f
Cecilia <cecilia0012@gmail.com>
2024-12-11 12:03:09 +0100

    cookie function added

--- core/templates/pages/learner-dashboard-page/language-banner/language-banner.component.ts
+++ core/templates/pages/learner-dashboard-page/language-banner/language-banner.component.ts
@@ -1,12 +1,33 @@
 import {Component, OnInit} from '@angular/core';
+import {CookieService} from 'ngx-cookie';
 
 @Component({
-  selector: 'app-language-banner',
+  selector: 'language-banner',
   templateUrl: './language-banner.component.html',
   styleUrls: ['./language-banner.component.scss'],
 })
 export class LanguageBannerComponent implements OnInit {
-  constructor() {}
+  isVisible: boolean;
+  isChecked: boolean = false;
+  constructor(private cookieService: CookieService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.cookieService.get('doNotShowLanguageBanner') !== 'true') {
+      this.isVisible = navigator.language.slice(0, 2) !== 'en';
+    }
+  }
+
+  onButtonClick() {
+    //this.isVisible = false;
+    if (this.isChecked) {
+      this.cookieService.put('doNotShowLanguageBanner', 'true');
+    }
+  }
 }
+
+// get language and check if en --
+// when got it is cliked get if checked --
+// when got it is clocked remove tooltip --
+// save coocie when button clicked if checked --
+// save lang pref if cases are specific.
+// unit tests
