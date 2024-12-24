bd0e7b984175ad8e4e58311fc996dd7ba100aed3
Mohit <mohitruwatia2312@gmail.com>
2024-12-24 18:21:13 +0530

    Removed console logs

--- core/templates/pages/exploration-editor-page/services/router.service.ts
+++ core/templates/pages/exploration-editor-page/services/router.service.ts
@@ -70,8 +70,6 @@ export class RouterService {
     this._changeTab(this.windowRef.nativeWindow.location.hash.split('#')[1]);
 
     this.location.onPopState(() => {
-      console.log(window.location.hash);
-      console.trace();
       if (window.location.hash === '') {
         setTimeout(() => {
           window.history.go(-1);
