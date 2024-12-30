b795e9b5ad84b968f247de0e00ac3b0ed295940c
Hemant <hemant.ayuj@gmail.com>
2024-12-30 22:25:27 +0530

    dummy story added

--- core/templates/domain/admin/admin-backend-api.service.ts
+++ core/templates/domain/admin/admin-backend-api.service.ts
@@ -650,6 +650,17 @@ export class AdminBackendApiService {
     });
   }
 
+  async generateDummyStoriesAsync(
+    TopicId: string,
+    numberOfStories: number
+  ): Promise<void> {
+    return this._postRequestAsync(AdminPageConstants.ADMIN_HANDLER_URL, {
+      action: 'generate_dummy_stories',
+      topic_id: TopicId,
+      num_dummy_stories_to_generate: numberOfStories,
+    });
+  }
+
   async retrieveExplorationInteractionIdsAsync(
     expId: string
   ): Promise<ExplorationInteractionIdsBackendResponse> {
