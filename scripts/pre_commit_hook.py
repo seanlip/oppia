7fba4864a3805f851a20c17349dde48d846ae11a
Jayam <jayampatel04@gmail.com>
2024-12-31 10:56:28 +0530

    Fixed pre-commit hook to work properly on Docker

--- scripts/pre_commit_hook.py
+++ scripts/pre_commit_hook.py
@@ -63,8 +63,7 @@ KEYS_UPDATED_IN_CONSTANTS: Final = [
     b'FIREBASE_CONFIG_APP_ID', b'FIREBASE_CONFIG_AUTH_DOMAIN',
     b'FIREBASE_CONFIG_MESSAGING_SENDER_ID', b'FIREBASE_CONFIG_PROJECT_ID',
     b'FIREBASE_CONFIG_STORAGE_BUCKET', b'FIREBASE_CONFIG_GOOGLE_CLIENT_ID']
-NPX_PATH: Final = os.path.join(
-    os.pardir, 'oppia_tools', 'node-16.13.0', 'bin', 'npx')
+NPX_PATH: Final = 'npx'
 
 
 def install_hook() -> None:
