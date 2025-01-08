c2036ddaf655e87a2db9f3ab68f442f18dff30e0
Ankita Saxena <ankitasonu24@gmail.com>
2025-01-08 13:55:59 +0000

    Fix exp_domain_test

--- core/domain/exp_domain_test.py
+++ core/domain/exp_domain_test.py
@@ -17945,7 +17945,11 @@ class ExplorationChangesMergeabilityUnitTests(
                 platform_parameter_list.ParamName.SYSTEM_EMAIL_ADDRESS,
                 'system@example.com'
             ),
-            (platform_parameter_list.ParamName.SYSTEM_EMAIL_NAME, '.')
+            (platform_parameter_list.ParamName.SYSTEM_EMAIL_NAME, '.'),
+            (
+                platform_parameter_list.ParamName.OPPIA_PROJECT_ID,
+                'dev-project-id'
+            )
         ]
     )
     def test_email_is_sent_to_admin_in_case_of_adding_deleting_state_changes(
@@ -18293,7 +18297,11 @@ class ExplorationChangesMergeabilityUnitTests(
                 platform_parameter_list.ParamName.SYSTEM_EMAIL_ADDRESS,
                 'system@example.com'
             ),
-            (platform_parameter_list.ParamName.SYSTEM_EMAIL_NAME, '.')
+            (platform_parameter_list.ParamName.SYSTEM_EMAIL_NAME, '.'),
+            (
+                platform_parameter_list.ParamName.OPPIA_PROJECT_ID,
+                'dev-project-id'
+            )
         ]
     )
     def test_email_is_sent_to_admin_in_case_of_state_renames_changes_conflict(
